314a58b624c6d85622a42fc247bb8d50a649a3b7
Celine <celine2005@yahoo.com>
2026-05-01 17:13:48 +0300

    fixed ui of stats

--- phase2/app/stats/page.jsx
+++ phase2/app/stats/page.jsx
@@ -11,27 +11,27 @@ export default async function StatsPage(){
         <main style={{ marginLeft: 70, padding: "2rem" }}>
         <h1>CAMDEE stats</h1>
         <div className="dashboard-grid">
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Total Users</h2>
                 <p>{totalUsers}</p>
             </div>
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Total Posts</h2>
                 <p>{totalPosts}</p>
             </div>
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Total Comments</h2>
                 <p>{totalComments}</p>
             </div>
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Average Followers</h2>
                 <p>{averageFollowers}</p>
             </div>
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Most Active User</h2>
                 <p>{activeUser?.username} - {activeUser?.posts} Posts</p>
             </div>
-            <div className="feed-card">
+            <div className="feed-cardStat">
                 <h2>Most Liked Post</h2>
                 <p>By {likedPost?.author} - {likedPost?.likes} Likes</p>
             </div>
